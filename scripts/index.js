2c490a00f4543403f329c2bad5886df2a99291f8
ob <oberezhinskiy@gmail.com>
2022-03-04 13:56:36 +0300

    10

--- scripts/index.js
+++ scripts/index.js
@@ -25,18 +25,129 @@ const next = document.querySelector('.bike-type__button-bar__button_right');
 const footerSearchBtn = document.querySelector('.footer__form__input-box__btn');
 const footerSearchInput = document.querySelector('.footer__form__input-box__input');
 
-const mediaQuery = window.matchMedia('(max-width: 320px)');
+
 const headerButton = document.querySelector('.header__button');
+const closeButton = document.querySelector('.header__mobile-navigator__close-button');
+const mobileNavigator = document.querySelector('.header__mobile-navigator');
 
-mediaQuery.addEventListener('change', (e) => {
-	if (e.matches) {
+const headerLinks = document.querySelector('#header_links');
+const headerLinksMobile = document.querySelector('#header_links_mobile');
 
-		console.log('Media Query Matched!');
+const footerSwitcher = document.querySelector('#footer_switcher');
+const footerSwitcherMobile = document.querySelector('#footer_switcher_mobile');
+
+const switchFooter = document.querySelector('#switch');
+const switchMobile = document.querySelector('#switch_mobile');
+
+const mediaQuery = window.matchMedia('(max-width: 320px)');
+
+const sectionTitles = document.querySelectorAll('.section-title');
+const sectionText = document.querySelectorAll('.section-text');
+const page = document.querySelector('.page');
+const main = document.querySelector('.main');
+const bikesCustomSelect = document.querySelector('.bikes__custom-select');
+const bikesHeaderLinksItem = document.querySelectorAll('.bikes__header__links__item');
+const bikesHeaderLinksItemInactive = document.querySelector('.bikes__header__links__item_inactive');
+const bikesSliderItemImg = document.querySelector('.bikes__slider__item__img');
+const bikesSliderItemName = document.querySelectorAll('.bikes__slider__item__name');
+const footer = document.querySelector('.footer');
+const footerСopyright = document.querySelector('.footer__copyright');
+const footerFormInputBoxBtn = document.querySelector('.footer__form__input-box__btn');
+const footerFormInputBoxInput = document.querySelector('.footer__form__input-box__input');
+const headerButtonLine = document.querySelectorAll('.header__button__line');
+const headerMobileNavigator = document.querySelector('.header__mobile-navigator');
+const infoBikeBlockImg = document.querySelector('.info__bike-block__img');
+const quoteDetailsAuthorSubtitle = document.querySelector('.quote__details__author__subtitle');
+const quoteDetailsTitle = document.querySelector('.quote__details__title');
+const quoteDetailsAuthorName = document.querySelector('.quote__details__author__name');
+const trainLinksItem = document.querySelectorAll('.train__links__item');
+const headerLink = document.querySelectorAll('.header__link');
+const switcherInputLabel = document.querySelectorAll('.switcher__input__label');
+const infoBikeBlockName = document.querySelector('.info__bike-block__name');
+const footerFormTitle = document.querySelector('.footer__form__title');
+const switcherButtonLight = document.querySelector('.switcher__button_light');
+const switcherButtonDark = document.querySelector('.switcher__button_dark');
+const headerMobileNavigatorCloseButton = document.querySelector('.header__mobile-navigator__close-button');
+const bikeTypeButtonBarButton = document.querySelectorAll('.bike-type__button-bar__button');
+const bikeTypeButtonBarButtonLeft = document.querySelector('.bike-type__button-bar__button_left');
+const bikeTypeButtonBarButtonRight = document.querySelector('.bike-type__button-bar__button_right');
+
+
+switchFooter.addEventListener('change', themeChange);
+switchMobile.addEventListener('change', themeChange);
+let isChecked = false;
+function themeChange(e) {
+
+	if (e.target.checked && isChecked || !e.target.checked && !isChecked) return;
+	if (e.target.id == switchMobile.id) {
+		switchFooter.checked = e.target.checked;
+	} else {
+		switchMobile.checked = e.target.checked;
 	}
-});
+	isChecked = e.target.checked;
+
+
+	sectionTitles.forEach(t => { t.classList.toggle("section-title__theme_dark"); });
+	sectionText.forEach(t => { t.classList.toggle("section-text__theme_dark"); });
+	switcherInputLabel.forEach(t => { t.classList.toggle("switcher__input__label__theme_dark"); });
+	trainLinksItem.forEach(t => { t.classList.toggle("train__links__item__theme_dark"); });
+	headerLink.forEach(t => { t.classList.toggle("header__link__theme_dark"); });
+	headerButtonLine.forEach(t => { t.classList.toggle("header__button__line__theme_dark"); });
+	bikesSliderItemName.forEach(t => { t.classList.toggle("bikes__slider__item__name__theme_dark"); });
+	bikesHeaderLinksItem.forEach(t => { t.classList.toggle("bikes__header__links__item__theme_dark"); });
+	bikeTypeButtonBarButton.forEach(t => { t.classList.toggle("bike-type__button-bar__button__theme_dark"); });
+
+	page.classList.toggle("page__theme_dark");
+	main.classList.toggle("main__theme_main");
+	bikesCustomSelect.classList.toggle("bikes__custom-select__theme_dark");
+	bikesHeaderLinksItemInactive.classList.toggle("bikes__header__links__item_inactive__theme_dark");
+	footer.classList.toggle("footer__theme_dark");
+	footerСopyright.classList.toggle("footer__copyright__theme_dark");
+	footerFormInputBoxBtn.classList.toggle("footer__form__input-box__btn__theme_dark");
+	footerFormInputBoxInput.classList.toggle("footer__form__input-box__input__theme_dark");
+
+	headerMobileNavigator.classList.toggle("header__mobile-navigator__theme_dark");
+	infoBikeBlockImg.classList.toggle("info__bike-block__img__theme_dark");
+	infoBikeBlockName.classList.toggle("info__bike-block__name__theme_dark");
+	quoteDetailsAuthorSubtitle.classList.toggle("quote__details__author__subtitle__theme_dark");
+	quoteDetailsTitle.classList.toggle("quote__details__title__theme_dark");
+	quoteDetailsAuthorName.classList.toggle("quote__details__author__name__theme_dark");
+	footerFormTitle.classList.toggle('footer__form__title__theme_dark');
+	switcherButtonDark.classList.toggle('switcher__button_dark__theme_dark');
+	switcherButtonLight.classList.toggle('switcher__button_light__theme_dark');
+	headerMobileNavigatorCloseButton.classList.toggle('header__mobile-navigator__close-button__theme_dark');
+	bikeTypeButtonBarButtonLeft.classList.toggle('bike-type__button-bar__button_left__theme_dark');
+	bikeTypeButtonBarButtonRight.classList.toggle('bike-type__button-bar__button_right__theme_dark');
+
+
+
+
+
+}
+
+function initFilter(e) {
+	if (e.matches) {
+		headerLinks.style.display = 'none';
+		footerSwitcher.style.display = 'none';
+	} else {
+		headerLinks.style.display = 'flex';
+		footerSwitcher.style.display = 'flex';
+		mobileNavigator.style.display = 'none';
+	}
+}
+initFilter(mediaQuery);
+
+mediaQuery.addEventListener("change", initFilter);
 
 headerButton.addEventListener('click', () => {
-	console.log('headerButton');
+	mobileNavigator.style.display = 'flex';
+	headerButton.style.display = 'none';
+
+});
+
+closeButton.addEventListener('click', () => {
+	mobileNavigator.style.display = 'none';
+	headerButton.style.display = 'block';
 });
 
 footerSearchInput.addEventListener('input', () => {
@@ -99,7 +210,7 @@ const swiperBike = new Swiper(".bikes", {
 	loop: true
 });
 
-const classList = ["url('../../../../../images/road_curve.svg')", "url('../../../../../images/gravel_curve.svg')", "url('../../../../../images/tt_curve.svg')"];
+const classList = ["url('../images/road_curve.svg')", "url('../images/gravel_curve.svg')", "url('../images/tt_curve.svg')"];
 const titleList = ['Шоссе', 'Грэвел', 'ТТ'];
 const textList = [
 	'На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.',
